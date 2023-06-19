be5988c0c89c60c22cbb1e3748278b6e80813381
rasyidf <ufarasfa@gmail.com>
2023-06-19 07:52:13 +0700

    Update Logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Search } from "@/components/groups/search";
 import { UserNav } from "@/components/groups/user-nav";
 import { BookPlus } from "lucide-react";
 import { Link, Outlet } from "react-router-dom";
+import logo from "@/assets/icon.png";
 
 export const Layout = () => {
     return (
@@ -11,7 +12,7 @@ export const Layout = () => {
                 <div className="border-b">
                     <div className="flex h-14 items-center px-4">
                         <Link to="/" className="text-2xl font-bold">
-                            <BookPlus className="h-6 w-6 mr-2" />
+                            <img src={logo} className="h-8" alt="Logo" />
                         </Link>
                         <MainNav className="mx-6" />
                         <div className="ml-auto flex items-center space-x-4">
